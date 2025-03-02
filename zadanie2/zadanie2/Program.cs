159ef3e83e7aee0d95d427b801a5021f5deeced4
s26519 <s26519@LAB223-06>
2025-03-02 18:14:23 +0100

    commit 2

--- zadanie2/zadanie2/Program.cs
+++ zadanie2/zadanie2/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("sample text");
+        string name = Console.ReadLine();
+        Console.WriteLine($"hi, {name}");
     }
 }
