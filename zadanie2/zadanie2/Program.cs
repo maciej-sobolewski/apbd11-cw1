8fa747390c5208c0a1526aa935061b4c72d10ef4
s26519 <s26519@LAB223-06>
2025-03-02 18:17:55 +0100

    commit 3

--- zadanie2/zadanie2/Program.cs
+++ zadanie2/zadanie2/Program.cs
@@ -7,5 +7,18 @@ class Program
         Console.WriteLine("sample text");
         string name = Console.ReadLine();
         Console.WriteLine($"hi, {name}");
+
+        Console.WriteLine("coinflip, type 'throw'");
+        string input = Console.ReadLine();
+        if (input == "throw")
+        {
+            Random random = new Random();
+            string result = random.Next(2) == 0 ? "heads" : "tails";
+            Console.WriteLine($"result: {result}");
+        }
+        else
+        {
+            Console.WriteLine("incorrect input");
+        }
     }
 }
